74de6596936a89279226acdf3df76381fca3fc99
exylaci <exy@freemail.hu>
2022-06-25 05:43:58 +0200

    Initial commit

---
+++ src/test/java/com/exy/nezok/NezokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exy.nezok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NezokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
